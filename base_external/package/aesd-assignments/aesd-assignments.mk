7118b4f2411f104de7419c16d4c47e96889a1b7c
ronli <ronli@Ubuntu.myguest.virtualbox.org>
2026-01-20 22:00:24 -0500

    Assignment-8-buildroot submissioin.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 5 git contents
-AESD_ASSIGNMENTS_VERSION = '1bfbd2fc3994c97da86cf6101201620ef16a8959'
+AESD_ASSIGNMENTS_VERSION = 'e42b4af26ed479bff8bf855d807bc14dcc72e265'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,15 +16,20 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) $(LINUX_MAKE_FLAGS) -C $(@D)/aesd-char-driver KERNELDIR=$(LINUX_DIR) modules
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5-buildroot/* $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket  $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+	$(INSTALL) -m 0644 $(@D)/aesd-char-driver/aesdchar.ko  $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)/aesdchar.ko
+	$(INSTALL) -m 0644 $(@D)/aesd-char-driver/aesdchar.ko  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
